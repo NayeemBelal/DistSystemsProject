7e9af21a81f14151a3c7de318e9f52b51c0205f4
NayeemBelal <nayeem.belal.1@gmail.com>
2023-12-02 13:24:45 -0600

    was

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
 
 user = "postgres"
-password = "Samie1204$%25"
+password = "Samie1204$%"
 DB_CONFIG = f"postgresql+asyncpg://{user}:{password}@localhost:5432/Login"
 
 
