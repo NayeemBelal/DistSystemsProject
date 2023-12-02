2a480a469c5627a1d1687375ea9ab5af97b760f2
NayeemBelal <nayeem.belal.1@gmail.com>
2023-12-02 13:09:03 -0600

    test

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
 
-DB_CONFIG = "postgresql+asyncpg://postgres:Samie1204$%@localhost:5432/Login"
+DB_CONFIG = "postgresql+asyncpg://nayeem:Samie1204$%@localhost:5432/Login"
 
 
 SECRET_KEY = "Samie1204"
